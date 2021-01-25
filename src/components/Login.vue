226073a757a23c1aba53fea820e0595066ef22d6
邓安月 <1434807906@qq.com>
2021-01-25 21:19:44 +0800

    add files

---
+++ src/components/Login.vue
@@ -0,0 +1,136 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <!-- 头像 -->
+      <div class="logo_box">
+        <img src="../assets/biglogo.jpg" alt="" />
+      </div>
+
+      <div class="title">应急服务平台后台管理系统</div>
+
+      <!-- 输入表单 -->
+      <el-form
+        ref="loginFormRef"
+        :model="loginForm"
+        class="login_form"
+        :rules="loginFormRules"
+      >
+        <!-- 用户名输入框 -->
+        <el-form-item label="" prop="username">
+          <!-- v-model="form.name".为每一个表单项绑定对应的数据 -->
+          <el-input
+            label="用户名"
+            v-model="loginForm.username"
+            prefix-icon="el-icon-user-solid"
+          >
+          </el-input>
+        </el-form-item>
+        <!-- 密码输入框 -->
+        <el-form-item label="" prop="password">
+          <el-input
+            label="密码"
+            type="password"
+            v-model="loginForm.password"
+            prefix-icon="el-icon-lock"
+          >
+          </el-input>
+        </el-form-item>
+        <!-- 按钮 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="reset" @click="resetLoginFrom">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      //登陆表单的数据绑定对象
+      loginForm: {
+        username: 'admin',
+        password: '123456',
+      },
+      //表单的验证规则
+      loginFormRules: {
+        username: [
+          { required: true, message: '请输入登录名称', trigger: 'blur' },
+          {
+            min: 3,
+            max: 10,
+            message: '长度在 3 到 10 个字符',
+            trigger: 'blur',
+          },
+        ],
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+      },
+    }
+  },
+  methods: {
+    resetLoginFrom() {
+      //resetFields()是在el-form中定义好的,this.$refs.loginFormRef是表单的实例对象,调用它的方法
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      this.$router.push('/home')
+    },
+  },
+}
+</script>
+
+<style lang="less" scoped>
+.login_container {
+  background-color: #e9edf1;
+  height: 100%;
+}
+
+.login_box {
+  position: absolute;
+  top: 150px;
+  left: 400px;
+  width: 500px;
+  height: 345px;
+  background-color: #b12412;
+  border-radius: 3px;
+}
+
+.logo_box {
+  position: absolute;
+  left: calc(50% - 65px);
+  top: -65px;
+  padding: 10px;
+  width: 130px;
+  height: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  background-color: #fff;
+  box-shadow: 0 0 10px #ddd; //加阴影
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background-color: #eee;
+  }
+}
+.title {
+  position: absolute;
+  width: 100%;
+  margin-top: 101px;
+  font-size: 25px;
+  color: #eee;
+  text-align: center;
+}
+
+.login_form {
+  position: absolute;
+  bottom: 0;
+  margin: 0 15px;
+  width: 90%;
+}
+.btns {
+  float: right;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,64 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Appraise from '../components/Information/Appraise.vue'
+import ResCall from '../components/Information/ResCall.vue'
+import Response from '../components/Information/Response.vue'
+import Map from '../components/Map/Map.vue'
+import Call from '../components/Call/Call.vue'
+import Resident from '../components/Resident/Resident.vue'
+import Apartment from '../components/Apartment/Apartment.vue'
+import User from '../components/System/User.vue'
+import Role from '../components/System/Role.vue'
+import Power from '../components/System/Power.vue'
+import Station from '../components/Station/Station.vue'
+import Fireman from '../components/Fireman/Fireman.vue'
+import Firecar from '../components/Firecar/Firecar.vue'
+import Data from '../components/Data/Data.vue'
 
 Vue.use(VueRouter)
+Vue.use(Login)
+Vue.use(Home)
+Vue.use(Welcome)
+Vue.use(Appraise)
+Vue.use(ResCall)
+Vue.use(Response)
+Vue.use(Call)
+Vue.use(Resident)
+Vue.use(Apartment)
+Vue.use(Map)
+Vue.use(User)
+Vue.use(Role)
+Vue.use(Power)
+Vue.use(Station)
+Vue.use(Fireman)
+Vue.use(Firecar)
+Vue.use(Data)
 
 const routes = [
+  { path: '/', redirect: '/login' },
+  { path: '/login', component: Login },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/home', component: Home, redirect: '/welcome',
+    children: [{ path: '/welcome', component: Welcome },
+    { path: '/rescall', component: ResCall },
+    { path: '/appraise', component: Appraise },
+    { path: '/response', component: Response },
+    { path: '/call', component: Call },
+    { path: '/resident', component: Resident },
+    { path: '/apartment', component: Apartment },
+    { path: '/map', component: Map },
+    { path: '/user', component: User },
+    { path: '/role', component: Role },
+    { path: '/power', component: Power },
+    { path: '/station', component: Station },
+    { path: '/fireman', component: Fireman },
+    { path: '/firecar', component: Firecar },
+    { path: '/data', component: Data },
+    ]
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
